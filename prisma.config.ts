d6e5fa00e2720a0577889aa185c55082466a5be3
Agustin Saez <agustin@kamini.cl>
2026-04-26 20:07:05 -0400

    Remove authToken from prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from 'prisma/config'
-import { config } from 'dotenv'
-
-config()
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  datasource: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  },
 })
