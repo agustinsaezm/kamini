ef50dd34d21b9a797df82e0b7f18948f112739c1
Agustin Saez <agustin@kamini.cl>
2026-04-26 20:02:20 -0400

    Fix PrismaLibSql casing

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
@@ -11,7 +11,7 @@ function createPrismaClient() {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
   })
-  const adapter = new PrismaLibSQL(client)
+  const adapter = new PrismaLibSql(client)
   return new PrismaClient({ adapter } as any) // eslint-disable-line @typescript-eslint/no-explicit-any
 }
 
